9dc996cb273d734ec38feab41b3b1eb017c2ffa4
SurajDesai <surajdesai1297@gmail.com>
2020-11-25 20:16:08 +0530

    Changed json return of error to 200

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -6,7 +6,7 @@ exports.createUser = (req, res) => {
     const errors = validationResult(req)
 
     if (!errors.isEmpty())
-        return res.status(400).json({ errors: errors.array() })
+        return res.status(200).json({ error: errors.errors[0] })
 
     let user = new User(req.body)
 
@@ -14,8 +14,7 @@ exports.createUser = (req, res) => {
 
     user.save((err, user) => {
         if (err) {
-            console.log(err)
-            return res.status(400).json({ error: "User not created" })
+            return res.status(200).json({ error: "User not created" })
         }
 
         res.json(user)
@@ -26,14 +25,15 @@ exports.siginUser = (req, res) => {
     const errors = validationResult(req)
 
     if (!errors.isEmpty())
-        return res.status(400).json({ errors: errors.array() })
+        return res.status(200).json({ error: errors.array()[0].msg })
 
     User.findOne({ email: req.body.email }, (err, user) => {
-        if (err) return res.status(400).json({ error: "Sigin unsuccessfull" })
+        if (err) return res.status(200).json({ error: "Sigin unsuccessfull" })
 
-        if (!user) return res.status(400).json({ error: "Email id not found" })
-        else if (!user.validatePassword(req.body.password))
-            res.status(400).json({ error: "Incorrect Password" })
+        if (!user) return res.status(200).json({ error: "User not found! Please check Email Id" })
+
+        if (!user.validatePassword(req.body.password))
+            return res.status(200).json({ error: "Incorrect Password" })
 
         var token = jwt.sign({ id: user._id }, user.salt)
 
